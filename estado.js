1681d37b9c813079178474d37e05660567ffb308
moisesdelonotos <moises.delossantosoy@uanl.edu.mx>
2026-01-21 16:13:19 -0800

    Update estado.js

--- estado.js
+++ estado.js
@@ -1,6 +1,1 @@
-const discord_webhook_url = "https://discord.com/api/webhooks/1447617665685393559/JFLb17ssEqVL_cgsBSd82Gwxb2tUAObW603zNFClsatK8okvQKWjOfgvkE-YG56kURUN";
 
-// Variables globales para compatibilidad con scripts existentes
-window.telegram_bot_estado = discord_webhook_url;
-window.chat_id_estado = "discord";
-window.discord_webhook_url = discord_webhook_url;
